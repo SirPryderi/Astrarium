cd96fa8f899b3b9519e4c0ad245009252874d013
Vittorio <pryderi.mail@gmail.com>
2017-04-07 16:59:59 +0100

    Added JavaDoc in Mathematics

--- src/astrarium/utils/Mathematics.java
+++ src/astrarium/utils/Mathematics.java
@@ -8,26 +8,53 @@ package astrarium.utils;
  * @author Vittorio
  */
 public final class Mathematics {
+    /**
+     * Makes the class non-instantiable.
+     */
     private Mathematics() {
     }
 
+    /**
+     * Returns the result of the inverse hyperbolic cosine.
+     *
+     * @param x value.
+     * @return inverse hyperbolic cosine of value.
+     */
     public static double acosh(double x) {
         return Math.log(x + Math.sqrt(x * x - 1));
     }
 
+    /**
+     * Returns the inverse sine function of alpha with the same sign of beta.
+     *
+     * @param alpha angle to determine the inverse of the cosine.
+     * @param beta  angle to determine the sign of the result.
+     * @return final result.
+     */
     public static double acos2(double alpha, double beta) {
         return Math.acos(alpha) * Math.signum(beta);
     }
 
-    public static double hypotenuse(double a, double b) {
-        return Math.sqrt(a * a + b * b);
+    /**
+     * A quick algorithm to compute an approximation of sqrt(<i>x</i><sup>2</sup>&nbsp;+<i>y</i><sup>2</sup>).
+     * <p>
+     * Not to use for high-precision calculations, use {@link Math#hypot(double, double)} instead.
+     *
+     * @param x first side.
+     * @param y second side.
+     * @return approximated hypotenuse.
+     */
+    public static double hypotenuseApprox(double x, double y) {
+        return y + 0.337 * x;
     }
 
-    public static double hypotenuseApprox(double a, double b) {
-        return b + 0.337 * a;
-    }
-
+    /**
+     * Normalise an angle in radians between the range -PI, PI.
+     *
+     * @param angle angle to normalise.
+     * @return normalise angle.
+     */
     public static double normaliseAngle(double angle) {
-        return angle % Math.PI * 2;
+        return Math.atan2(Math.sin(angle), Math.cos(angle));
     }
 }
