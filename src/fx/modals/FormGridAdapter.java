5a6d80ff85a26b5ec928da490710e981ef742d0a
Vittorio <pryderi.mail@gmail.com>
2017-04-11 16:27:56 +0100

    Created a class for populating grid panels with a form

---
+++ src/fx/modals/FormGridAdapter.java
@@ -0,0 +1,62 @@
+package fx.modals;
+
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A utility class used to populate a grid pane with a format containing label and node in two different columns.
+ * Created on 11/04/2017.
+ *
+ * @author Vittorio
+ */
+class FormGridAdapter {
+    /**
+     * The grid pane the adapter is attached to.
+     */
+    private final GridPane gridPane;
+    /**
+     * The starting row.
+     */
+    private int offset = 0;
+
+    /**
+     * Initialise the adapter and stars from row 0.
+     *
+     * @param gridPane the grid pane to populate.
+     */
+    FormGridAdapter(@NotNull GridPane gridPane) {
+        this.gridPane = gridPane;
+    }
+
+    /**
+     * Initialise the adapter and starts from an arbitrary row {@code offset}.
+     *
+     * @param gridPane the grid pane to populate.
+     * @param offset   the starting row.
+     */
+    FormGridAdapter(@NotNull GridPane gridPane, int offset) {
+        if (offset < 0)
+            throw new IllegalArgumentException("Start must be greater than 0");
+
+        this.gridPane = gridPane;
+        this.offset = offset;
+    }
+
+    /**
+     * Adds the row with label to the next available slot.
+     *
+     * @param label text label of the row.
+     * @param node  content of the row, usually an input of some sort.
+     */
+    void addToGrid(@NotNull String label, @NotNull Node node) {
+        Text text = new Text(label);
+
+        gridPane.add(text, 0, offset);
+        gridPane.add(node, 1, offset);
+
+        offset++;
+    }
+
+}
